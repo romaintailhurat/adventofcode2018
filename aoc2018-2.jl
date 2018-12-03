c8b35410024d23797966a31e443f35d37ed9cf5a
romaintailhurat <romain.tailhurat@gmail.com>
2018-12-03 22:40:53 +0100

    [Puzzle 2] Solving 2.2 using cosine distance

--- aoc2018-2.jl
+++ aoc2018-2.jl
@@ -1,4 +1,9 @@
 #= Solving https://adventofcode.com/2018/day/2 =#
+using Distances
+using DataStructures
+
+const intToLetter = Dict(collect(enumerate("abcdefghijklmnopqrstuvwxyz")))
+const letterToInt = Dict(value => key for (key, value) in intToLetter)
 
 function letterCount(s)
     counter = Dict{Char, Int}()
@@ -6,6 +11,10 @@ function letterCount(s)
         counter[letter] = get(counter, letter, 0) + 1
     end
     return counter
+end
+
+function getVectorFromString(s)
+    return map(letter -> letterToInt[letter], collect(s))
 end
 
 function solvePartOne(lines)
@@ -24,10 +33,33 @@ function solvePartOne(lines)
     println(twoAndThrees[2] * twoAndThrees[3])
 end
 
+function solvePartTwo(lines)
+    cosinePairs = DataStructures.SortedDict()
+    for line in lines
+        for line2 in lines
+            if line != line2
+                dist = evaluate(CosineDist(), getVectorFromString(line), getVectorFromString(line2))
+                key = line * "-" * line2
+                cosinePairs[key] = dist
+            end
+        end
+    end
+    
+    vs = collect(values(cosinePairs))
+    ks = collect(keys(cosinePairs))
+    min, indexOfMin = findmin(vs)
+    winnerPair= getindex(ks, indexOfMin)
+    println(winnerPair)
+    s1, s2 = split(winnerPair, "-")
+    commonElements = intersect(s1, s2)
+    println(join(commonElements))
+end
+
 # ----
 input = open("inputs/aoc2018-2.txt")
 lines = readlines(input)
 
-solvePartOne(lines)
+#solvePartOne(lines)
+solvePartTwo(lines)
 
 close(input)
