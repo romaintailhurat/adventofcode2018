cbc3ee1b6350b2cd49adb31f05a970c9ee3e17dc
romaintailhurat <romain.tailhurat@gmail.com>
2018-12-03 20:25:51 +0100

    [Puzzle 1] Non efficient solution for second part

--- aoc2018-1.jl
+++ aoc2018-1.jl
@@ -3,12 +3,12 @@ https://adventofcode.com/2018/day/1
 """
 
 function parseOperation(line)
-    return (line[1], line[2:end])
+    return (line[1], parse(Int, line[2:end]))
 end
 
-function red(acc, curr)
-    operator, s_value = parseOperation(curr)
-    value = parse(Int, s_value)
+#@test parseOperation("-120") == ("-", 120)
+
+function sumOrSub(operator, acc, value)
     if operator == '+'
         return acc + value
     else
@@ -16,12 +16,40 @@ function red(acc, curr)
     end
 end
 
+const initFrequency = 0
+
+function red(acc, curr)
+    operator, value = parseOperation(curr)
+    return sumOrSub(operator, acc, value)
+end
+
 input = open("inputs/aoc2018-1-1.txt")
-
 lines = readlines(input)
-
-res = reduce(red, lines; init = 0)
-
-print(res)
-
 close(input)
+
+# Subproblem 1
+"""
+res = reduce(red, lines; init = initFrequency)
+print(res, "\n")
+"""
+
+# Subproblem 2
+frequencies = []
+currentFreq = 0
+
+for (i, v) in enumerate(Iterators.cycle(lines))    
+    operator, value = parseOperation(v)
+    global currentFreq
+    newFreq = sumOrSub(operator, currentFreq, value)
+
+    if i % 1000 == 0
+        print(newFreq, " - ", currentFreq, " - ", length(frequencies), "\n")
+    end
+
+    if newFreq in frequencies
+        print("Already seen! ", newFreq, "\n")
+        break
+    end
+    push!(frequencies, newFreq)
+    currentFreq = newFreq
+end
